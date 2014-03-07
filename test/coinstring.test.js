32dd07afe9d2a47d6103880d10f11feedc582829
JP Richardson <jprichardson@gmail.com>
2014-03-07 11:03:58 -0600

    add Dogecoin tests

--- test/coinstring.test.js
+++ test/coinstring.test.js
@@ -5,18 +5,19 @@ require('terst');
 
 
 describe('coinstring', function() {
+  var privateKeyHex = "1184cd2cdd640ca42cfc3a091c51d549b2f016d454b2774019c2b2d2e08529fd";
+  var privateKeyHexBuf = conv(privateKeyHex, {in: 'hex', out: 'buffer'});
+  var privateKeyHexCompressed = privateKeyHex + "01"; //<--- compression involves appending 0x01 to end
+  var privateKeyHexCompressedBuf = conv(privateKeyHexCompressed, {in: 'hex', out: 'buffer'});
+  var hash160 = "3c176e659bea0f29a3e9bf7880c112b1b31b4dc8"; //hash representing uncompressed
+  var hash160Buf = conv(hash160, {in: 'hex', out: 'buffer'});
+  var hash160c = "a1c2f92a9dacbd2991c3897724a93f338e44bdc1"; //hash representing compressed
+  var hash160cBuf = conv(hash160c, {in: 'hex', out: 'buffer'});
+
   describe('> when all arguments are passed', function() {
     describe('> when Bitcoin', function() {
-      var hash160 = "3c176e659bea0f29a3e9bf7880c112b1b31b4dc8"; //hash representing uncompressed
-      var hash160Buf = conv(hash160, {in: 'hex', out: 'buffer'});
-      var hash160c = "a1c2f92a9dacbd2991c3897724a93f338e44bdc1"; //hash representing compressed
-      var hash160cBuf = conv(hash160c, {in: 'hex', out: 'buffer'});
       var address = "16UjcYNBG9GTK4uq2f7yYEbuifqCzoLMGS"
       var addressCompressed = '1FkKMsKNJqWSDvTvETqcCeHcUQQ64kSC6s';
-      var privateKeyHex = "1184cd2cdd640ca42cfc3a091c51d549b2f016d454b2774019c2b2d2e08529fd";
-      var privateKeyHexBuf = conv(privateKeyHex, {in: 'hex', out: 'buffer'});
-      var privateKeyHexCompressed = privateKeyHex + "01"; //<--- compression involves appending 0x01 to end
-      var privateKeyHexCompressedBuf = conv(privateKeyHexCompressed, {in: 'hex', out: 'buffer'});
       var wif = "5Hx15HFGyep2CfPxsJKe2fXJsCVn5DEiyoeGGF6JZjGbTRnqfiD"
       var wifCompressed = 'KwomKti1X3tYJUUMb1TGSM2mrZk1wb1aHisUNHCQXTZq5auC2qc3';
 
@@ -41,6 +42,51 @@ describe('coinstring', function() {
 
       describe('> when private', function() {
         var version = 0x80 //bitcoin private key
+
+        it('coinstring should generate the WIF', function() {
+          EQ (coinstring(version, privateKeyHexBuf), wif);
+          EQ (coinstring(version, privateKeyHexCompressedBuf), wifCompressed);
+        })
+
+        it('validate should validate the address', function() {
+          T (coinstring.validate(version, wif));
+          T (coinstring.validate(version, wifCompressed));  
+        })
+
+        it('decode should parse the address and return the hash160', function() {
+          EQ (coinstring.decode(version, wif).toString('hex'), privateKeyHex);
+          EQ (coinstring.decode(version, wifCompressed).toString('hex'), privateKeyHexCompressed);   
+        })
+      })
+    })
+
+    describe('> when Dogecoin', function() {
+      var address = "DAcq9oJpZZAjr56RmF7Y5zmWboZWQ4HAsW"
+      var addressCompressed = 'DKtQu8G1cFQikveWy3qAkQTDMY8PKVU18Z';
+      var wif = "6JGLNEiEYR6pFGq84gwceHWYLcyKaLWzaymVajjCPPUEGAR2MTT"
+      var wifCompressed = 'QPCgUjWzmfNfXzsQBHJ4KZsPKbmaz99PAyZP9ubFFpBBXWuSQh6n';
+
+      describe('> when public', function() {
+        var version = 0x1E //dogecoin public / pubkeyhash
+
+        it('coinstring should generate the public address', function() {
+          EQ (coinstring(version, hash160Buf), address);
+          EQ (coinstring(version, hash160cBuf), addressCompressed);          
+        })
+
+        it('validate should validate the address', function() {
+          T (coinstring.validate(version, address));
+          T (coinstring.validate(version, addressCompressed));  
+        })
+
+        it('decode should parse the address and return the hash160', function() {
+          EQ (coinstring.decode(version, address).toString('hex'), hash160);
+          EQ (coinstring.decode(version, addressCompressed).toString('hex'), hash160c);   
+        })
+      })
+
+      describe('> when private', function() {
+        var version = 0x1E + 0x80 //dogecoin private key
 
         it('coinstring should generate the WIF', function() {
           EQ (coinstring(version, privateKeyHexBuf), wif);
