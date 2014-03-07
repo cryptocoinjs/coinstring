59e5e197b5c8304482ba997fd9e71f8fa97c589d
JP Richardson <jprichardson@gmail.com>
2014-03-07 10:24:16 -0600

    basic implementation complete

--- test/coinstring.test.js
+++ test/coinstring.test.js
@@ -7,35 +7,54 @@ require('terst');
 describe('coinstring', function() {
   describe('> when all arguments are passed', function() {
     describe('> when Bitcoin', function() {
+      var hash160 = "3c176e659bea0f29a3e9bf7880c112b1b31b4dc8"; //hash representing uncompressed
+      var hash160Buf = conv(hash160, {in: 'hex', out: 'buffer'});
+      var hash160c = "a1c2f92a9dacbd2991c3897724a93f338e44bdc1"; //hash representing compressed
+      var hash160cBuf = conv(hash160c, {in: 'hex', out: 'buffer'});
+      var address = "16UjcYNBG9GTK4uq2f7yYEbuifqCzoLMGS"
+      var addressCompressed = '1FkKMsKNJqWSDvTvETqcCeHcUQQ64kSC6s';
+      var privateKeyHex = "1184cd2cdd640ca42cfc3a091c51d549b2f016d454b2774019c2b2d2e08529fd";
+      var privateKeyHexBuf = conv(privateKeyHex, {in: 'hex', out: 'buffer'});
+      var privateKeyHexCompressed = privateKeyHex + "01"; //<--- compression involves appending 0x01 to end
+      var privateKeyHexCompressedBuf = conv(privateKeyHexCompressed, {in: 'hex', out: 'buffer'});
+      var wif = "5Hx15HFGyep2CfPxsJKe2fXJsCVn5DEiyoeGGF6JZjGbTRnqfiD"
+      var wifCompressed = 'KwomKti1X3tYJUUMb1TGSM2mrZk1wb1aHisUNHCQXTZq5auC2qc3';
+
       describe('> when public', function() {
-        it('should generate the public address', function() {
-          var hash160 = "3c176e659bea0f29a3e9bf7880c112b1b31b4dc8"; //hash representing uncompressed
-          var hash160Buf = conv(hash160, {in: 'hex', out: 'buffer'});
-          var address = "16UjcYNBG9GTK4uq2f7yYEbuifqCzoLMGS"
-          var version = 0x0 //bitcoin public / pubkeyhash
+        var version = 0x0 //bitcoin public / pubkeyhash
+
+        it('coinstring should generate the public address', function() {
           EQ (coinstring(version, hash160Buf), address);
+          EQ (coinstring(version, hash160cBuf), addressCompressed);          
+        })
 
-          //EQ (coinstring.decode(version, address).toString('hex'), hash160Buf.toString('hex'));
+        it('validate should validate the address', function() {
+          T (coinstring.validate(version, address));
+          T (coinstring.validate(version, addressCompressed));  
+        })
 
-          hash160 = "a1c2f92a9dacbd2991c3897724a93f338e44bdc1"; //hash representing compressed
-          hash160Buf = conv(hash160, {in: 'hex', out: 'buffer'});
-          address = '1FkKMsKNJqWSDvTvETqcCeHcUQQ64kSC6s';
-          EQ (coinstring(version, hash160Buf), address)
+        it('decode should parse the address and return the hash160', function() {
+          EQ (coinstring.decode(version, address).toString('hex'), hash160);
+          EQ (coinstring.decode(version, addressCompressed).toString('hex'), hash160c);   
         })
       })
 
       describe('> when private', function() {
-        it('should generate the WIF', function() {
-          var privatekeyhex = "1184cd2cdd640ca42cfc3a091c51d549b2f016d454b2774019c2b2d2e08529fd";
-          var wif = "5Hx15HFGyep2CfPxsJKe2fXJsCVn5DEiyoeGGF6JZjGbTRnqfiD"
-          var version = 0x80 //bitcoin private key
+        var version = 0x80 //bitcoin private key
 
-          EQ (coinstring(version, conv(privatekeyhex, {in: 'hex', out: 'buffer'})), wif);
+        it('coinstring should generate the WIF', function() {
+          EQ (coinstring(version, privateKeyHexBuf), wif);
+          EQ (coinstring(version, privateKeyHexCompressedBuf), wifCompressed);
+        })
 
-          //compressed
-          var wifCompressed = 'KwomKti1X3tYJUUMb1TGSM2mrZk1wb1aHisUNHCQXTZq5auC2qc3';
-          privatekeyhex += '01'; //<--- '01' at the end represents a compressed wif address
-          EQ (coinstring(version, conv(privatekeyhex, {in: 'hex', out: 'buffer'})), wifCompressed);
+        it('validate should validate the address', function() {
+          T (coinstring.validate(version, wif));
+          T (coinstring.validate(version, wifCompressed));  
+        })
+
+        it('decode should parse the address and return the hash160', function() {
+          EQ (coinstring.decode(version, wif).toString('hex'), privateKeyHex);
+          EQ (coinstring.decode(version, wifCompressed).toString('hex'), privateKeyHexCompressed);   
         })
       })
     })
