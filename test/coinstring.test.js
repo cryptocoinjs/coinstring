af2288de1d50b5b8008c88aa9c978a1982697157
JP Richardson <jprichardson@gmail.com>
2014-06-06 03:34:43 -0500

    small cleanup

--- test/coinstring.test.js
+++ test/coinstring.test.js
@@ -8,7 +8,7 @@ describe('coinstring', function() {
     fixtures.valid.forEach(function(f) {
       it('should encode ' + f.description, function() {
         var inp = new Buffer(f.hex, 'hex')
-        var version = parseInt(f.versionHex, '16')
+        var version = parseInt(f.version, '16')
         assert.equal(cs.encode(inp, version), f.base58)
         assert.equal(cs.encode([].slice.call(inp), version), f.base58)
         assert.equal(cs.encode(new Uint8Array([].slice.call(inp)), version), f.base58)
@@ -19,7 +19,7 @@ describe('coinstring', function() {
   describe('+ decode()', function() {
     fixtures.valid.forEach(function(f) {
       it('should decode ' + f.description, function() {
-        var version = parseInt(f.versionHex, '16')
+        var version = parseInt(f.version, '16')
         assert.equal(cs.decode(f.base58, version).payload.toString('hex'), f.hex)
         assert.equal(cs.decode(f.base58).payload.toString('hex'), f.hex)
       })
@@ -29,7 +29,7 @@ describe('coinstring', function() {
       fixtures.invalid.forEach(function(f) {
         it(f.description + ' should throw an error', function() {
           assert.throws(function() {
-            var version = parseInt(f.versionHex, '16')
+            var version = parseInt(f.version, '16')
             assert(cs.decode(version, f.base58))
           })
         })
@@ -40,7 +40,7 @@ describe('coinstring', function() {
   describe('+ isValid()', function() {
     fixtures.valid.forEach(function(f) {
       it('should validate ' + f.description, function() {
-        var version = parseInt(f.versionHex, '16')
+        var version = parseInt(f.version, '16')
         assert(cs.isValid(f.base58, version))
       })
     })
@@ -48,7 +48,7 @@ describe('coinstring', function() {
     describe('> when invalid input', function() {
       fixtures.invalid.forEach(function(f) {
         it(f.description + ' should return false', function() {
-          var version = parseInt(f.versionHex, '16')
+          var version = parseInt(f.version, '16')
           assert(!cs.isValid(f.base58, version))
         })
       })
@@ -59,7 +59,7 @@ describe('coinstring', function() {
     fixtures.valid.forEach(function(f) {
       it('should create an encoder ' + f.description, function() {
         var inp = new Buffer(f.hex, 'hex')
-        var version = parseInt(f.versionHex, '16')
+        var version = parseInt(f.version, '16')
 
         var encode = cs.createEncoder(version)
         assert.equal(encode(inp), f.base58)
@@ -71,7 +71,7 @@ describe('coinstring', function() {
     fixtures.valid.forEach(function(f) {
       it('should create a decoder ' + f.description, function() {
         var inp = new Buffer(f.hex, 'hex')
-        var version = parseInt(f.versionHex, '16')
+        var version = parseInt(f.version, '16')
 
         var decode = cs.createDecoder(version)
         assert.equal(decode(f.base58).payload.toString('hex'), f.hex)
@@ -82,7 +82,7 @@ describe('coinstring', function() {
   describe('+ createValidator()', function() {
     fixtures.valid.forEach(function(f) {
       it('should create a validator ' + f.description, function() {
-        var version = parseInt(f.versionHex, '16')
+        var version = parseInt(f.version, '16')
         var isValid = cs.createValidator(version)
         assert(isValid(f.base58))
       })
@@ -91,7 +91,7 @@ describe('coinstring', function() {
     describe('> when invalid input', function() {
       fixtures.invalid.forEach(function(f) {
         it(f.description + ' should return false', function() {
-          var version = parseInt(f.versionHex, '16')
+          var version = parseInt(f.version, '16')
           var isValid = cs.createValidator(version)
           assert(!isValid(f.base58))
         })
